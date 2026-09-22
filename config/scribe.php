d104f09f8f27bf1c9cbbcb60460b54c3733bd83b
Beatrix Ducz <dgloria.web@gmail.com>
2026-09-22 19:26:40 +0100

    first

--- config/scribe.php
+++ config/scribe.php
@@ -25,8 +25,7 @@ return [
         INTRO,
 
     // The base URL displayed in the docs.
-    // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    // 'base_url' => config('app.url'),
+    'base_url' => rtrim(env('APP_URL', 'http://localhost'), '/'),
 
     // Routes to include in the docs
     'routes' => [
@@ -52,10 +51,10 @@ return [
     ],
 
     // The type of documentation output to generate.
-    // - "static" will generate a static HTMl page in the /public/docs folder,
+    // - "static" will generate a static HTML page in the /public/docs folder,
     // - "laravel" will generate the documentation as a Blade view, so you can add routing and authentication.
     // - "external_static" and "external_laravel" do the same as above, but pass the OpenAPI spec as a URL to an external UI template
-    'type' => 'laravel',
+    'type' => 'static',
 
     // See https://scribe.knuckles.wtf/laravel/reference/config#theme for supported options
     'theme' => 'default',
@@ -68,7 +67,7 @@ return [
 
     'laravel' => [
         // Whether to automatically create a docs route for you to view your generated docs. You can still set up routing manually.
-        'add_routes' => true,
+        'add_routes' => false,
 
         // URL path to use for the docs endpoint (if `add_routes` is true).
         // By default, `/docs` opens the HTML page, `/docs.postman` opens the Postman collection, and `/docs.openapi` the OpenAPI spec.
@@ -164,7 +163,7 @@ return [
                     'key' => 'baseUrl',
                     'type' => 'string',
                     'name' => 'string',
-                    'value' => 'http://localhost:8081',
+                    'value' => rtrim(env('APP_URL', 'http://localhost:8083'), '/'),
                 ],
                 [
                     'id' => 'email',
